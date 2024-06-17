61050959d866072320e396331018cc8ac63f51c0
olubukola@zestpayment.com <olubukola@zestpayment.com>
2024-06-17 22:05:31 +0100

    updated changes

--- src/providers/auth-provider/index.tsx
+++ src/providers/auth-provider/index.tsx
@@ -1,48 +1,45 @@
-'use client'
+'use client';
 
-import React, {FC, ReactNode, useEffect} from 'react';
-import {axios} from '@/utils/axios-config'
-
+import React, { FC, ReactNode, useEffect } from 'react';
+import { axios } from '@/utils/axios-config';
 
 type AuthProviderProps = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
-const AuthProvider: FC<AuthProviderProps> = (props) => {
-    const {children} = props
+const AuthProvider: FC<AuthProviderProps> = props => {
+  const { children } = props;
 
+  //   const interceptor = axios.interceptors.response.use(
+  axios.interceptors.response.use(
+    response => {
+      console.log('runs here');
+      return response;
+    },
+    async error => {
+      if (error.response && error.response.status === 401) {
+        try {
+          // Perform token refresh or re-authentication
+          // Example:
+          // const refreshedToken = await refreshToken();
+          // Save refreshed token to localStorage or cookies
+          // axios.defaults.headers.common['Authorization'] = `Bearer ${refreshedToken}`;
+          // Retry the original request
 
-        const interceptor = axios.interceptors.response.use(
-            response => response,
-            async error => {
-                if (error.response && error.response.status === 401) {
-                    try {
-                        // Perform token refresh or re-authentication
-                        // Example:
-                        // const refreshedToken = await refreshToken();
-                        // Save refreshed token to localStorage or cookies
-                        // axios.defaults.headers.common['Authorization'] = `Bearer ${refreshedToken}`;
-                        // Retry the original request
-                        return axios.request(error.config);
-                    } catch (refreshError) {
-                        // Handle refresh error or redirect to login page
-                        // Example:
-                        // redirectToLogin();
-                        return Promise.reject(refreshError);
-                    }
-                }
-                return Promise.reject(error);
-            }
-        );
+          return axios.request(error.config);
+        } catch (refreshError) {
+          // Handle refresh error or redirect to login page
+          // Example:
+          // redirectToLogin();
+          return Promise.reject(refreshError);
+        } finally {
+        }
+      }
+      return Promise.reject(error);
+    },
+  );
 
-
-
-
-    return (
-        <>
-            {children}
-        </>
-    );
+  return <>{children}</>;
 };
 
 export default AuthProvider;
